eb0f4aa82257ed72357d19536021a24a120e7103
sayuri.hamada.gp <sayuri.hamada@gpol.co.jp>
2022-01-07 15:39:32 +0900

    [update] /dist/配下コミット

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
+
   {
     path: '/',
     name: 'Home',
